aa5e3389243d45690c0c3095d21a45bad9b1a80e
SahilTyagii <tyagi28sahil@gmail.com>
2024-05-10 13:17:45 +0530

    completed posts page and added create and read posts functionality

--- server.js
+++ server.js
@@ -3,13 +3,70 @@ import bodyParser from "body-parser"
 
 const app = express()
 const port = 3000
-var blogs = []
+var blogs = {
+    
+}
+
+// for (const[name, content] of Object.entries(blogs)) {
+//     console.log(`${name}: ${content}`)
+// }
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs")
+})
+
+app.post("/submit", (req, res) => {
+    const heading = req.body["title"]
+    const data = req.body["data"]
+    // console.log(`title: ${heading} data: ${data}`)
+    blogs[heading] = data
+
+    var title = []
+    var content = []
+    var wordCount = []
+    for (const[name, data] of Object.entries(blogs)) {
+        title.push(name)
+        content.push(data)
+        wordCount.push(data.split(" ").length)
+    }
+    res.render("posts.ejs", {
+        names: title,
+        blogs: content,
+        words: wordCount,
+    })
+})
+
+app.post("/read", (req, res) => {
+    // console.log(`body: ${req.body["title"]}`)
+    const name = req.body["title"]
+    res.render("read.ejs", {
+        title: name,
+        content: blogs[name]
+    })
+})
+
+app.get("/posts", (req, res) => {
+    // for (var member in blogs) delete blogs[member];
+    var title = []
+    var content = []
+    var wordCount = []
+    for (const[name, data] of Object.entries(blogs)) {
+        title.push(name)
+        content.push(data)
+        wordCount.push(data.split(" ").length)
+    }
+    res.render("posts.ejs", {
+        names: title,
+        blogs: content,
+        words: wordCount,
+    })
 })
 
 app.listen(port, () => {
